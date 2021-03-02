656d039fa776986e51f0fd5535815d19d1be2ab8
JR Ladd <johnrobertladd@gmail.com>
2021-03-02 14:49:59 -0600

    final revisions to word2vec

--- _build/jupyter_execute/word2vec.py
+++ _build/jupyter_execute/word2vec.py
@@ -9,7 +9,7 @@
 # 
 # You can decide how deep you'd like to go into the weeds of the ways Word2Vec actually produces its similarities. For a first introduction, I recommend [Jay Alammar's *The Illustrated Word2Vec*](https://jalammar.github.io/illustrated-word2vec/), which includes lots of helpful visualizations as it explains the basics. In this tutorial, we'll focus on training Word2Vec in Python and interpreting the results, rather than reviewing the underlying concepts.
 # 
-# Let's begin by importing necessarily libraries:
+# Let's begin by importing the necessarily libraries:
 
 # In[1]:
 
@@ -26,7 +26,7 @@ from matplotlib import pyplot as plt
 # 
 # Word2Vec creates vectors based on which words are near one another. We'll train our model on lists of words from a selected corpus, and we'll use the same corpus we've been using all along: the *EarlyPrint* texts published in 1666.
 # 
-# We're using the [Gensim](https://radimrehurek.com/gensim/models/word2vec.html) library to create our Word2Vec model. Gensim accepts several different types of input, but we'll focus on giving it lists of word tokens. You could create a list of all the tokens in a text and pass them directly to Gensim, but Gensim prefers if you give it each sentence separately. This is because sentence boundaries often contain information about word relationships: the last word of the previous sentence and the first word of the next one don't have the same relationship as two words in the same sentence.
+# We're using the [Gensim](https://radimrehurek.com/gensim/models/word2vec.html) library to create our Word2Vec model. Gensim accepts several different types of input, but we'll focus on giving it lists of word tokens. You could create a list of all the tokens in a text and pass them directly to Gensim, but Gensim prefers that you give it each sentence separately. This is because sentence boundaries often contain information about word relationships: the last word of a given sentence and the first word of the next one don't have the same relationship as two words in the same sentence.
 # 
 # Below we create a function for finding the lemmas in every sentence of our texts, which then collects those sentences as individual lists. For more detail on how this code works, refer to [our XML tutorial](https://earlyprint.org/jupyterbook/ep_xml.html#step-4-lines-stanzas-and-sentences).
 
@@ -61,9 +61,9 @@ for f in files:
 # 
 # Now that we have a list of all the lemmas in all the sentences in our texts, we're ready to train our Word2Vec model. This can be done with the simple one-line command below.
 # 
-# The parameter `min_count` refers to the minimum amount of times a word must appear in the corpus in order to be part of the model. For the sake of speed, I'm eliminating all words that appear less than 2 times. (And since the eliminated words appear only once, the resulting vectors wouldn't be very reliable anyway: not enough examples of adjacent words.)
+# The parameter `min_count` refers to the minimum number of times a word must appear in the corpus in order to be part of the model. For the sake of speed, I'm eliminating all words that appear less than 2 times. (And since the eliminated words appear only once, the resulting vectors wouldn't be very reliable anyway: not enough examples of adjacent words.)
 # 
-# The parament `window` refers to the "sliding window" that Word2Vec pulls across a sentence to determine if words are near each other. The default window is 5 words. In general, Word2Vec gives better results in a very large corpus, when there are lots of instances of every word. For our *EarlyPrint* applications of Word2Vec, we'll train the model on the full corpus. But for this sample one-year corpus, let's shrink the window to just 4 words: by capturing words that are a little closer together we reduce the likelihood of a one-off adjacency skewing our results.
+# The parameter `window` refers to the "sliding window" that Word2Vec pulls across a sentence to determine if words are near each other. The default window is 5 words. In general, Word2Vec gives better results in a very large corpus, when there are lots of instances of every word. For our *EarlyPrint* applications of Word2Vec, we'll train the model on the full corpus. But for this sample one-year corpus, let's shrink the window to just 4 words, which will generate more windows across our smaller corpus.
 # 
 # Once we've selected parameters we can train our Word2Vec model.
 
@@ -81,7 +81,7 @@ word2vec = Word2Vec(all_sentences, min_count=2, window=4)
 print(list(word2vec.wv.vocab)[:50])
 
 
-# Using the `wv` object, it's simple to retrieve the words most similar to a particular word of your choice. Looking at the first 50 words in the corpus above, we'll choose a word with special resonance for 1666, the year of the Great Fire of London: "flame." Let's find the words most similar to the word "flame".
+# Using the `wv` object, it's simple to retrieve the words most similar to a particular word of your choice. Looking at the first 50 words in the corpus above, we'll choose a word with special resonance for 1666, the year of the Great Fire of London: "flame." Let's find the words most similar to the word "flame," where similarity refers to the likelihood that the word would appear in contexts measurably like the ones in which "flame" appears.
 
 # In[6]:
 
@@ -93,7 +93,7 @@ print(word2vec.wv.most_similar("flame"))
 # 
 # The values given with each word are its cosine similarity to the source word. (See our [Similarity tutorial](https://earlyprint.org/jupyterbook/similarity.html) for more about this.)
 # 
-# If you had a list of words you were particularly interested in, perhaps organized around a theme, you could easily look at the most similar words to each one and begin to collect a semantic field for the topics that interest you.
+# If you had a list of words you were particularly interested in, perhaps organized around a theme, you could easily look at the most similar words to each one and begin to populate the semantic field of the topic that interests you.
 # 
 # ## Accessing and Visualizing Vectors
 # 
@@ -105,7 +105,7 @@ print(word2vec.wv.most_similar("flame"))
 print(word2vec.wv["flame"])
 
 
-# Each word in the text has a vector of the same length. Using these vectors, we can recreate some of the illustrations from Alammar's [The Illustrated Word2Vec](https://jalammar.github.io/illustrated-word2vec/).
+# Each word in the text has a vector of the same length of 100 features, or dimensions, though the value of each feature will be different for every word. Using these 100-dimension vectors, we can recreate some of the illustrations from Alammar's [The Illustrated Word2Vec](https://jalammar.github.io/illustrated-word2vec/).
 # 
 # Let's start with "flame" and three similar words: "cloud," "fire," and "smoke." We can put the vectors of each of these words into a `pandas` DataFrame and then visualize them as a heatmap. 
 # 
@@ -179,4 +179,4 @@ plt.show()
 
 # This is much better. We can see how the words are positioned relative to one another. Though we might have expected to seem them next to each other, remember that PCA is only showing us a reduced representation of our high-dimensional data. Still, we can see that "smoke" and "flame" are closer to each other than "cloud" and "fire." And all four words are almost in a horizontal line across the graph, suggesting they're quite similar according to principal component 2 (the y-axis), but perhaps less similar according to PC1 (the x-axis). Finally, by comparing the last graph to this one, we can also see where these words fit in areas of semantic density (lots of similar words) versus sparseness (words with more distinct meanings).
 # 
-# As text similarity increased our sense of text-level relationships in the last tutorial, Word2Vec gives us a clearer (but not complete) sense of word-level relationships. Stacked with other methods, Word2Vec can be used to explore themes and subjects, to help computers and human readers make sense of semantic distinctions, and to drive complex language-based machine learning algorithms.
+# As text similarity increased our sense of text-level relationships in the Similarity tutorial, Word2Vec gives us a clearer (but not complete) sense of word-level relationships. Stacked with other methods, Word2Vec can be used to explore themes and subjects, to help computers and human readers make sense of semantic distinctions, and to drive complex language-based machine learning algorithms.
